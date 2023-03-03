c661692a5e5693425f443efb7cb367e6bef6c8fd
Joshua_Arfa <soondevjosh@gmail.com>
2023-03-03 16:29:45 -0600

    Working in views(already working) and the routes

--- src/routes/links.js
+++ src/routes/links.js
@@ -8,4 +8,46 @@ router.get( '/add', (req, res) => {
     res.render('links/add');
 });
 
+router.post('/add', async (req, res) => {
+    const { title, url, description } = req.body;
+    const newLink = {
+        title,
+        url,
+        description
+    };
+    pool.query('INSERT INTO links set ?', [ newLink ]);
+    req.flash('success', 'Link saved successfully');
+    res.redirect('/links');
+});
+
+router.get('/', async(req, res) => {
+    const links = await pool.query('SELECT * FROM links');
+    console.log(links);
+    res.render( 'links/list', { links } );
+});
+
+router.get('/delete/:id', async (req, res) => {
+    const { id } = req.params;
+    await pool.query('DELETE FROM links WHERE ID = ?', [ id ]);
+    res.redirect('/links')
+});
+
+router.get('/edit/:id', async (req, res) => {
+    const { id } = req.params;
+    const links = await pool.query('SELECT * FROM links WHERE id = ?', [ id ]);
+    res.render('links/edit', {link: links[0]});
+});
+
+router.post('/edit/:id', async (req, res) => {
+    const { id } = req.params;
+    const { title, url, description } = req.body;
+    const newLink = {
+        title,
+        url,
+        description
+    };
+    await pool.query('UPDATE links set ? WHERE id = ?', [ newLink, id ]);
+    res.redirect('/links');
+});
+
 module.exports = router;
